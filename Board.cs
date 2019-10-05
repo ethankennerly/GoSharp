a649755180957914cf51bf64ddbaf2428b807eba
Ethan Kennerly <ethankennerly@gmail.com>
2019-10-05 10:24:53 -0700

    Board: Self-point is not a liberty.

--- Board.cs
+++ Board.cs
@@ -309,12 +309,15 @@ namespace Go
         /// <summary>
         /// Gets the liberty count of the specified group.
         /// </summary>
-        /// <param name="group">The group object.</param>
+        /// <param name="group">The group object.
+        /// If group is empty subtracts 1 point.
+        /// Otherwise, playing a stone in an empty 1x1 board counts the destination as having a liberty.
+        /// </param>
         /// <returns>The number of liberties of the specified group.</returns>
         public int GetLiberties(Group group)
         {
             if (group.Content == Content.Empty)
-                return group.Points.Count();
+                return group.Points.Count() - 1;
 
             int libs = 0;
             foreach (var n in group.Neighbours)
